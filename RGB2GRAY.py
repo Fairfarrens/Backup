11d90dc744eb759bfe12b08aae18b4808005e7c9
m0_74260710 <m0_74260710@noreply.gitcode.com>
2024-07-11 16:19:55 +0800

    step2

--- RGB2GRAY.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-image1=cv2.imread(r"C:\Users\Fairfarren\Desktop\pearl.jpg")
-cv2.imshow("Origin",image1)
-#RGB色彩空间转GRAY色彩空间
-image2=cv2.cvtColor(image1,cv2.COLOR_RGB2GRAY)
-cv2.imshow("Gray",image2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
